3fea80f3e4125baa1fb86b0f3a2bfa5cfb7660b4
KingzRex <toochukwukingz6@gmail.com>
2023-10-30 11:51:00 +0100

    feat:project setup

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+require("solidity-coverage");
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      chainId: 80001,
+      accounts: [PRIVATE_KEY],
+    },
+    avalancheFujiTestnet: {
+      url: "https://avalanche-fuji-c-chain.publicnode.com",
+      chainId: 43113,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    url: "https://rpc.ankr.com/polygon_mumbai",
+    apiKey: {
+      polygonMumbai: "RZ2RSK3GIMZHPGF57NN42A9PZQET8K4FD6",
+    },
+  },
+};
